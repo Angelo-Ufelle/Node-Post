96265d6afcd04c8fd77c55f6c76c7341a0a02a62
Angelo-Ufelle <onyedikaufelle@gmail.com>
2022-11-05 16:51:01 +0100

    update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.post('/', (req, res) => {
 
   if (isNaN(x) || isNaN(y))
     return res.send({
-      error: "Stop being sneaky, put in the right parameters"
+      error: "Wrong values"
     })
 
   let result = ''
